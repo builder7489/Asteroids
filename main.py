9c81d80fc03b14ef450e56c33c4a7921c041e2c0
builder7489 <larrydeontucker@protonmail.com>
2025-09-05 22:27:25 -0400

    added player object not functional

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 # using magic numbers in the game
 from constants import *
+from player import Player
 
 def main():
     pygame.init
@@ -11,14 +12,31 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # player starting location
+    x_location = SCREEN_WIDTH / 2
+    y_location = SCREEN_HEIGHT / 2
+
+    player = Player(x_location, y_location, PLAYER_RADIUS)   
+
+    # Start game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        # render player
+        player.draw(screen)
+
         pygame.display.flip
+
+        game_clock.tick(60)
+        dt = game_clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,29 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.x = x
+        self.y = y
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
