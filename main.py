2b6d7d079de1aacdae4432b0e64b465e919f5afc
builder7489 <larrydeontucker@protonmail.com>
2025-09-23 16:29:19 -0400

    added player shooting feature

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -19,22 +20,21 @@ def main():
     game_clock = pygame.time.Clock()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    # player starting location
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
 
     # Groups for the game loop
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group() 
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    # Add groups to all player objects
+    # Adding groups to objects
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
-    player = Player(x, y)
+    # Add player with center starting point
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
     # Start game loop
@@ -61,8 +61,6 @@ def main():
                 sys.exit()
 
         pygame.display.flip()
-
-
 
 if __name__ == "__main__":
     main()
